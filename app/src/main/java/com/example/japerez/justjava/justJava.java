7bec0d8cb33b91d3838905b0233af5ad0a1e250f
Javier Perez <34148619+javtech95@users.noreply.github.com>
2018-08-29 11:15:27 -0400

    created initial views

---
+++ app/src/main/java/com/example/japerez/justjava/justJava.java
@@ -0,0 +1,13 @@
+package com.example.japerez.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class justJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_just_java);
+    }
+}
